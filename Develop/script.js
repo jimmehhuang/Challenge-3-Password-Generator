c5addb6a779290f6e66536a1fb4f3bc843717222
unknown <jimmymhuang@gmail.com>
2022-09-30 20:23:33 -0500

    finishing edits to javascript and README file

--- Develop/script.js
+++ Develop/script.js
@@ -1,5 +1,76 @@
-// Assignment code here
 
+//function to get a random INTEGER between min and max
+//***INCLUSIVE SELECTION!!!
+function randomInt(min, max){
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  return Math.floor(Math.random()*(max-min+1)+min);
+}
+//function to get an item from a random position in any given list
+function randomItem(list) {
+  return list[randomInt(0, list.length-1)];
+}
+//overall password generator function
+function generatePassword() {
+//checks if user actually input numbers
+  var userInput = prompt("Please enter how long you want your password to be, between 8 and 128 characters.");
+  var passLength = parseInt(userInput);
+
+  if (isNaN(passLength)) {
+    alert("Please enter a number between 8 and 128.")
+    return;
+  }
+//checks if inputted length is between 8 and 128
+  if ((passLength < 8) || (passLength > 128)) {
+    alert("Password length must be between 8 and 128 characters.")
+    return;
+  }
+//window box for each type of character, returns boolean
+  var numChars = confirm("Would you like to include numbers?");
+  var lowChars = confirm("Would you like to include lowercase letters?");
+  var upChars = confirm("Would you like to include UPPERCASE letters?");
+  var symChars = confirm("Would you like to include symbols?");
+//ends process if no character types are included
+  if (!numChars && !lowChars && !upChars && !symChars){
+    alert("Please select at least one type of character to include.");
+    return;
+  }
+//list of all possible characters...
+  var numList = ["0","1","2","3","4","5","6","7","8","9"];
+  var lowcharList = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
+//loop to convert each lowercase letter to uppercase so that we don't have to type each one out  
+  var upcharList = [];
+  for (var x=0; x< lowcharList.length; x++) {
+    upcharList[x] = lowcharList[x].toUpperCase();
+  }
+  var symcharList = ["!","@","#","$","%","^","&","*","(",")"];
+//combine arrays of characters that user has chosen to use, into 1 big array
+  var mastercharList = [];
+
+  if (numChars === true) {
+    mastercharList.push(numList);
+  }
+  if (lowChars === true) {
+    mastercharList.push(lowcharList);
+  }
+  if (upChars === true) {
+    mastercharList.push(upcharList);
+  }
+  if (symChars === true) {
+    mastercharList.push(symcharList);
+  }
+
+//select random item from the master list (mastercharlist), then place into each spot in the array
+  var generatedPass = "";
+
+  for (var x=0; x<passLength; x++) {
+    var randomList = randomItem(mastercharList);
+    var randomChar = randomItem(randomList);    
+    generatedPass += randomChar
+  }
+
+  return generatedPass;
+}
 
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
--- README.md
+++ README.md
@@ -1,1 +1,27 @@
 # Password Generator Starter Code
+
+## Description
+
+This password generator was given as a starter code for practice on writing functions and window alerts. 
+
+## Usage
+
+Clicking the "Generate Password" button will prompt the user to confirm how long they want their password to be, between 8 and 128 characters. Entering a non-number or a number outside of this range will cause a window alert to re-prompt the user to type an integer between 8 and 128.
+
+Once a number has been entered, the window alert boxes will prompt the user to select whether they want to include numbers, lowercase letter, uppercase letters, or symbols in the password. Failing to select any of the boxes will cause the window alert to prompt the user to please select at least one type of character.
+
+The generated password should appear in the box in the center of the screen.
+
+## License
+
+Refer to license in repo or as below:
+
+MIT License
+
+Copyright (c) 2022 jimmehhuang
+
+Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
